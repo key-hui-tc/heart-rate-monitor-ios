23ca3dec79fb3ec6f7e8b788193859fdb81ee139
Key Hui <>
2021-10-26 23:13:39 +0800

    feat: initial commit

---
+++ HeartRateMonitor/HeartRateMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeartRateMonitorApp.swift
+//  HeartRateMonitor
+//
+//  Created by Key Hui on 10/26/21.
+//
+
+import SwiftUI
+
+@main
+struct HeartRateMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
