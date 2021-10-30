414fc4ab98cf52b08a19d162b986f7bb57d312c3
Key Hui <>
2021-10-30 12:06:05 +0800

    feat: add a dummy test

--- HeartRateMonitorUITests/HeartRateMonitorUITests.swift
+++ HeartRateMonitorUITests/HeartRateMonitorUITests.swift
@@ -22,13 +22,13 @@ class HeartRateMonitorUITests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // UI tests must launch the application that they test.
+    func testEmptyLogin() throws {
         let app = XCUIApplication()
         app.launch()
 
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        app.buttons["LOGIN"].tap()
+        let lblError = app.staticTexts["Information not correct. Try again."]
+        XCTAssertTrue(lblError.waitForExistence(timeout: 1))
     }
 
     func testLaunchPerformance() throws {
